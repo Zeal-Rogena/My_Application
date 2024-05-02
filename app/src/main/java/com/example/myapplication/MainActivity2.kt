8504bc43909ded0b5a4369178a38e813ea29331e
Zeal-Rogena <zealrogena@gmail.com>
2024-05-02 22:32:26 +0300

    one night and a few mins later

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,91 @@
+package com.example.myapplication
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.view.animation.AnticipateOvershootInterpolator
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val loginButton = binding.login
+        val createButton = binding.create
+
+        val loginAnimator = ObjectAnimator.ofFloat(loginButton, "translationY", 100f, 0f)
+        val createAnimator = ObjectAnimator.ofFloat(createButton, "translationY", 100f, 0f)
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(loginAnimator, createAnimator)
+        animatorSet.duration = 2000
+        animatorSet.interpolator = AnticipateOvershootInterpolator()
+        animatorSet.start()
+
+        auth = Firebase.auth
+
+        binding.login.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            if (checkAllFields()) {
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val jay = Intent(this@MainActivity2, MainActivity4::class.java)
+                        startActivity(jay)
+                        finish()
+                    } else {
+                        Log.e("error", it.exception.toString())
+                    }
+                }
+            }
+        }
+        binding.create.setOnClickListener {
+            val kay = Intent(this@MainActivity2, MainActivity3::class.java)
+            startActivity(kay)
+            finish()
+        }
+
+    }
+    private fun checkAllFields():Boolean{
+        val email = binding.email.text.toString()
+        if (binding.email.text.toString()==""){
+            binding.emailayout.error = "Please input ur Email"
+            return false
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.emailayout.error = "Please input ur email using the right format"
+            return false
+        }
+        if (binding.password.text.toString() ==""){
+            binding.passwordlayout.error = "Please enter Password"
+            binding.passwordlayout.errorIconDrawable = null
+            return false
+        }
+        if(binding.password.length() <=8){
+            binding.passwordlayout.error = "Passoword is too short "
+            binding.passwordlayout.errorIconDrawable = null
+        }
+        return true
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity4.kt
@@ -0,0 +1,61 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.databinding.ActivityMain3Binding
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity4 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain3Binding
+    private lateinit var quizModelList : MutableList<QuizModel>
+    private lateinit var adapter: QuizListAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding = ActivityMain3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        quizModelList = mutableListOf()
+        getDataFromFirebase()
+
+
+    }
+
+    private fun setupRecyclerView(){
+        binding.progressBar.visibility = View.GONE
+        adapter = QuizListAdapter(quizModelList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun getDataFromFirebase(){
+        binding.progressBar.visibility = View.VISIBLE
+        FirebaseDatabase.getInstance().reference
+            .get()
+            .addOnSuccessListener { dataSnapshot->
+                if(dataSnapshot.exists()){
+                    for (snapshot in dataSnapshot.children){
+                        val quizModel = snapshot.getValue(QuizModel::class.java)
+                        if (quizModel != null) {
+                            quizModelList.add(quizModel)
+                        }
+                    }
+                }
+                setupRecyclerView()
+            }
+
+
+    }
+}
+
